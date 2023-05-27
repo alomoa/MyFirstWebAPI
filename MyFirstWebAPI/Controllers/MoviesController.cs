212e3b76442e3988ab8409630446c8a38d3e4935
Alom <alom@lothea.co.uk>
2023-05-27 12:16:05 +0100

    Add project files.

---
+++ MyFirstWebAPI/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebAPI.context;
+using MyFirstWebAPI.Models;
+
+namespace MyFirstWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MediaContext _dbContext;
+
+        public MoviesController(MediaContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet(Name = "GetMovies")]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            //confused to why we need to encase our IEnumerable with action result 
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            return await _dbContext.Movies.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if (_dbContext.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _dbContext.Movies.FindAsync(id);
+
+            if(movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+        [HttpPost]
+        public async Task<ActionResult> PostMovie(Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new {id = movie.Id}, movie);
+        }
+    }
+}
---
+++ MyFirstWebAPI/DatabaseContext/MediaContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MyFirstWebAPI.Models;
+
+namespace MyFirstWebAPI.context;
+
+public partial class MediaContext : DbContext
+{
+    public MediaContext()
+    {
+    }
+
+    public MediaContext(DbContextOptions<MediaContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Media;Trusted_Connection=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Movie__3214EC074566B300");
+
+            entity.ToTable("Movie");
+
+            entity.Property(e => e.Genre)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.ReleaseDate).HasColumnType("datetime");
+            entity.Property(e => e.Title)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
